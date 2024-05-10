b17100dd612a49a28108ec2843a987df74b92589
Zhennimadaijin <165613851+Zhennimadaijin@users.noreply.github.com>
2024-05-10 11:05:19 +0800

    Update axi_stream_insert_header_optimized_tb.v

--- axi_stream_insert_header_optimized_tb.v
+++ axi_stream_insert_header_optimized_tb.v
@@ -11,13 +11,13 @@ reg                            rst_n;
 
 reg                            valid_insert;
 reg   [DATA_WD-1 : 0]          data_insert;
-reg   [DATA_BYTE_WD-1 : 0] 	keep_insert;
+reg   [DATA_BYTE_WD-1 : 0] keep_insert;
 wire                           ready_insert;
 reg   [BYTE_CNT_WD-1 : 0]      byte_insert_cnt; 
 
   
 reg                        	    valid_in;
-reg   [DATA_WD-1 : 0]        	data_in;
+reg   [DATA_WD-1 : 0]         data_in;
 reg   [DATA_BYTE_WD-1 : 0] 	    keep_in;
 reg                        	    last_in;
 wire                       	    ready_in;
